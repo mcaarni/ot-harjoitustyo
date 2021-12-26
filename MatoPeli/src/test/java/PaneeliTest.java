406a8d7ebd4c1e887e64ce9fdf853fbe5f55d40c
Aarni Manninen <aarnimanninen7@gmail.com>
2021-12-26 20:20:38 +0200

    Lopullinen työ

--- MatoPeli/src/test/java/PaneeliTest.java
+++ MatoPeli/src/test/java/PaneeliTest.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 
+import MatoPeli.ui.Paneeli;
+import java.awt.Graphics;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,16 +8,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author aarni
- */
 public class PaneeliTest {
 
     Paneeli p;
-
-    public PaneeliTest() {
-    }
+    Graphics g;
 
     @Before
     public void setUp() {
@@ -27,11 +19,63 @@ public class PaneeliTest {
     }
 
     @Test
-    public void toimiikoTormays() {
-
+    public void toimiikoVasenTormays() {
+        p.aloitaPeli();
+        p.onkoTormays();
+        if (p.getX() < 0) {
+            assertEquals("false", p.getLiikkuu());
+        }
     }
 
     @Test
-    public void toimiikoKaannyOikealle() {
+    public void toimiikoOikeaTormays() {
+        p.aloitaPeli();
+        p.onkoTormays();
+        if (p.getX() > p.getSW()) {
+            assertEquals("false", p.getLiikkuu());
+        }
     }
+
+    @Test
+    public void toimiikoYlaTormays() {
+        p.aloitaPeli();
+        p.onkoTormays();
+        if (p.getY() < 0) {
+            assertEquals("false", p.getLiikkuu());
+        }
+    }
+
+    @Test
+    public void toimiikoAlaTormays() {
+        p.aloitaPeli();
+        p.onkoTormays();
+        if (p.getY() > p.getSH()) {
+            assertEquals("false", p.getLiikkuu());
+        }
+    }
+
+    @Test
+    public void toimiikoNapataanNami() {
+        p.aloitaPeli();
+        if (p.getX() == p.getNamiX() && p.getY() == p.getNamiY()) {
+            assertEquals("1", p.getMontaSyoty() + 1);
+        }
+    }
+
+    @Test
+    public void onkoPituusAlussaOk() {
+        p.aloitaPeli();
+        assertEquals(6, p.getOsat());
+    }
+
+    @Test
+    public void ylosLiikuttaaYlos() {
+        p.aloitaPeli();
+        p.liiku();
+        if (p.getSuunta() == 'U') {
+            assertEquals('U', p.getSuunta());
+        }
+    }
+
+
 }
